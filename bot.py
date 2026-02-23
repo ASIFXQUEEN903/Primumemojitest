3293a6ecc870a012f96e85d52cd1c97ba9100399
RAJ RAUTTRAY <jasminbibiworkweb45@gmail.com>
2026-02-24 02:19:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,30 +8,23 @@ BOT_TOKEN = "8373170298:AAEVgziTeeHsKBCCK2X-_7pCcJ_kaeroz5A"
 
 CUSTOM_EMOJI_ID = 6289579292565178263
 
-app = Client(
-    "emoji_bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+app = Client("emoji_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @app.on_message(filters.command("start"))
 async def start_handler(client, message):
 
-    base_text = "Hi I am custom emoji "
-    text = base_text + "A"
+    text = "Hi I am custom emoji 🙂"
 
     await message.reply_text(
         text,
         entities=[
             MessageEntity(
-                type=MessageEntityType.CUSTOM_EMOJI,   # 🔥 FIX HERE
-                offset=len(base_text),
+                type=MessageEntityType.CUSTOM_EMOJI,
+                offset=len("Hi I am custom emoji "),
                 length=1,
                 custom_emoji_id=CUSTOM_EMOJI_ID
             )
         ]
     )
 
-print("Bot Started...")
 app.run()
